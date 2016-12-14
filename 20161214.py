fc375ee4b804d79775abab800f75768c511e07c7
weitingforyou <liang0962054.dif02@g2.nctu.edu.tw>
2016-12-14 20:46:59 +0800

    Create 20161214.py

---
+++ 20161214.py
@@ -0,0 +1,69 @@
+import numpy as np
+from numpy import array, zeros, argmin, inf
+from matplotlib import pyplot as plt
+from sklearn.metrics.pairwise import euclidean_distances
+
+def dtw(x, y, dist):
+    """
+    Computes Dynamic Time Warping (DTW) of two sequences.
+    :param array x: N1*M array
+    :param array y: N2*M array
+    :param func dist: distance used as cost measure
+    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
+    """
+    assert len(x)
+    assert len(y)
+    r, c = len(x), len(y)
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = D0[1:, 1:] # view
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] = dist(x[i], y[j])
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    if len(x)==1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
+    
+    
+def _traceback(D):
+    i, j = array(D.shape) - 2
+    p, q = [i], [j]
+    while ((i > 0) or (j > 0)):
+        tb = argmin((D[i, j], D[i, j+1], D[i+1, j]))
+        if (tb == 0):
+            i -= 1
+            j -= 1
+        elif (tb == 1):
+            i -= 1
+        else: # (tb == 2):
+            j -= 1
+        p.insert(0, i)
+        q.insert(0, j)
+    return array(p), array(q)
+
+i = 1
+dataset = 'Signal_' + str(i) + '.txt'
+DataMat1 = np.loadtxt(dataset, delimiter=' ')
+dataset = 'Signal_' + str(i+1) + '.txt'
+DataMat2 = np.loadtxt(dataset, delimiter=' ')
+
+dist_fun = euclidean_distances
+
+dist, cost, acc, path = dtw(DataMat1, DataMat2, dist_fun)
+
+
+print dist
+
+from matplotlib import pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(path[0], path[1],'b')
